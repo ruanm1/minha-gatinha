af66ec1298e1460bef832be6675a4f9331152ba4
Ruan Montenegro <159306525+ruanm1@users.noreply.github.com>
2026-06-18 22:43:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.getElementById("abrir");
+
+botao.addEventListener("click",()=>{
+
+    document.getElementById("cap1").scrollIntoView({
+
+        behavior:"smooth"
+
+    });
+
+});
